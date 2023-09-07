bd094f7388067688a088e94330a171d169d6f13e
cyounseock <youns@kakao.com>
2023-09-07 18:44:30 +0900

    rought

--- database.py
+++ database.py
@@ -1,5 +1,3 @@
-import contextlib
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -12,3 +10,10 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ frontend/src/rotues/Home.svelte
@@ -0,0 +1,21 @@
+<script>
+    import fastapi from "../lib/api";
+    import {link} from 'svelte-spa-router'
+    import { createEventDispatcher } from "svelte";
+
+    let question_list = []
+
+    function get_question_list() {
+        fastapi('get', '/api/question/list', {}, (JSON) => {
+            question_list = JSON
+        })
+    }
+
+    get_question_list()
+</script>
+
+<u1>
+    {#each question_list as question}
+        <li><a use:link href="/detail/{question.id}">{question.subject}</a></li>
+    {/each}
+</u1>
