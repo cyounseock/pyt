4b29ee1d34a3972d6b9ccbd1607983011f21e750
cyounseock <youns@kakao.com>
2023-09-27 12:12:11 +0900

    230927

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,10 +1,13 @@
 <script>
     import fastapi from "../lib/api"
     import { link } from 'svelte-spa-router'
+    import { page } from "../lib/store"
+    import moment from 'moment/min/moment-with-locales'
+    moment.locale('ko')
 
     let question_list = []
     let size = 10
-    let page = 0
+
     let total = 0
     $: total_page = Math.ceil(total/size)
 
@@ -15,12 +18,12 @@
         }
         fastapi('get', '/api/question/list', params, (json) => {
             question_list = json.question_list
-            page = _page
+            $page = _page
             total = json.total
         })
     }
 
-    get_question_list(0)
+    $: get_question_list($page)
 </script>
 
 <div class="container my-3">
@@ -35,11 +38,14 @@
         <tbody>
         {#each question_list as question, i}
         <tr>
-            <td>{i+1}</td>
+            <td>{total - ($page * size) - i}</td>
             <td>
                 <a use:link href="/detail/{question.id}">{question.subject}</a>
+                {#if question.answers.length > 0}
+                <span class="text-danger small mx-2">[{question.answers.length}]</span>
+                {/if}
             </td>
-            <td>{question.create_date}</td>
+            <td>{moment(question.create_date).format("YYYY년 MM월 DD일 hh:mm a")}</td>
         </tr>
         {/each}
         </tbody>
@@ -47,20 +53,20 @@
     <!-- 페이징처리 시작 -->
     <ul class="pagination justify-content-center">
         <!-- previus page-->
-        <li class="page-item {page <= 0 && 'disabled'}">
-            <button class="page-link" on:click="{() => get_question_list(page-1)}">back</button>
+        <li class="page-item {$page <= 0 && 'disabled'}">
+            <button class="page-link" on:click="{() => get_question_list($page-1)}">back</button>
         </li>
         <!--page number-->
         {#each Array(total_page) as _, loop_page}
-        {#if loop_page >= page-2 && loop_page <= page+2}
-        <li class="page-item {loop_page === page && 'active'}">
+        {#if loop_page >= $page-2 && loop_page <= $page+2}
+        <li class="page-item {loop_page === $page && 'active'}">
             <button on:click="{() => get_question_list(loop_page)}" class="page-link">{loop_page+1}</button>
         </li>            
         {/if}
         {/each}
         <!--next page-->
-        <li class="page-item {page >= total_page-1 && 'disabled'}">
-            <button class="page-link" on:click="{() => get_question_list(page+1)}">next</button>
+        <li class="page-item {$page >= total_page-1 && 'disabled'}">
+            <button class="page-link" on:click="{() => get_question_list($page+1)}">next</button>
         </li>
     </ul>
     <!-- 페이징처리 끝 -->
